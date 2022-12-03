8015c427458be7c3bfd3008b482b6bf13eec0825
Mangelol <magnus1112@hotmail.com>
2022-12-03 16:59:51 +0100

    mockup for profile

--- src/views/profileView.jsx
+++ src/views/profileView.jsx
@@ -9,6 +9,7 @@ function ProfileView(props) {
         in the second larger div, show achievements
     */
     return <div className="profileView">
+        <span>
         <span className="profileInfo">
             <img className="profilePic" src="./loid.jpg" alt="" />
             <br></br>
@@ -19,8 +20,41 @@ function ProfileView(props) {
             9001 seasons played
            
         </span>
-        <h1>In progress</h1>
-        <h3>Profile details etc going to be displayed here</h3>
+        <span>
+            <h2>Last 5 seasons</h2>
+              
+                <span className="seasonsProfile">Score</span>
+                <span className="seasonsProfile">Difficulty</span>
+                <span className="seasonsProfile">Time</span>
+                <br></br>
+                <span className="seasonsProfile">Over 9000</span>
+                <span className="seasonsProfile">Hard</span>
+                <span className="seasonsProfile">25 seconds</span>
+                <br></br>
+                <span className="seasonsProfile">Over 9000</span>
+                <span className="seasonsProfile">Hard</span>
+                <span className="seasonsProfile">25 seconds</span>
+                <br></br>
+                <span className="seasonsProfile">Over 9000</span>
+                <span className="seasonsProfile">Hard</span>
+                <span className="seasonsProfile">25 seconds</span>
+                <br></br>
+                <span className="seasonsProfile">Over 9000</span>
+                <span className="seasonsProfile">Hard</span>
+                <span className="seasonsProfile">25 seconds</span>
+                <br></br>
+                <span className="seasonsProfile">Over 9000</span>
+                <span className="seasonsProfile">Hard</span>
+                <span className="seasonsProfile">25 seconds</span>
+
+                <h3>last 5 seasons above needs some obvious styling work</h3>
+               
+        </span>
+        </span>
+        <span>
+            <h2>Achievements</h2>
+            display achievements here
+        </span>
     </div>
 }
 
